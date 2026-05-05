bf3ee8d77e7c8fb46f27567703555831475a8e76
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2026-05-05 14:20:08 -0400

    Add space after icons in blocks.def.h
    Added a space after each icon for better readability.

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,15 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"🐧", 	"~/scripts/jt-kernel",		3600,		0},
+	{"🐧 ", 	"~/scripts/jt-kernel",		3600,		0},
 
-	{"💿", 	"~/scripts/jt-disk",		60,		0},
+	{"💿 ", 	"~/scripts/jt-disk",		60,		0},
 
-	{"🧠",	"~/scripts/jt-memory",		30,		0},
+	{"🧠 ",	"~/scripts/jt-memory",		30,		0},
 
-	{"📅", 	"~/scripts/jt-date",		5,		0},
+	{"📅 ", 	"~/scripts/jt-date",		5,		0},
 
-	{"🕛", 	"~/scripts/jt-time",		5,		0},
+	{"🕛 ", 	"~/scripts/jt-time",		5,		0},
 
 	{"", 	"~/scripts/jt-battery",		60,		0},
 };
