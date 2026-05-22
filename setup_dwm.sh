ff8fbf50f14c0a21df7b1e4ba2e94a48af845a7a
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2026-05-22 12:04:05 -0400

    Remove 'fish' from package installation list
    Removed 'fish' from the list of packages to install.

--- setup_dwm.sh
+++ setup_dwm.sh
@@ -1,5 +1,5 @@
 ##### PACKAGES
-fish eza git vim neovim gcc make base-devel pkgconf xorg
+git vim gcc make base-devel pkgconf xorg
 feh gxmessage sxiv 
 terminus-font ttf-mononoki-nerd noto-fonts-emoji lightdm lightdm-gtk-greeter 
 openssh firefox qutebrowser mousepad figlet sxhkd \
