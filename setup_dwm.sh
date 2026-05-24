410a85481809e942d634b5e05686542a99041395
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2026-05-24 18:43:29 -0400

    Remove openssh setup from setup_dwm.sh
    Removed installation and configuration for openssh.

--- setup_dwm.sh
+++ setup_dwm.sh
@@ -1,14 +1,1 @@
 
-terminus-font
-ttf-mononoki-nerd 
-
-openssh
-
-##### openssh -- Enable and start openssh
-echo "Enabling and starting openssh..."
-sleep 5
-sudo systemctl enable sshd
-sudo systemctl start sshd
-sudo ufw allow ssh
-sleep 5
-
